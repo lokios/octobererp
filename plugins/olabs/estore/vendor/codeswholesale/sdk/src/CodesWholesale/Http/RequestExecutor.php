c69e1757ddfbc1bd537852f1e4fc4b9fcf8f4f55
Anuj Sinha <anutech17.gmail.com>
2018-04-06 14:28:59 +0530

    Version Updates

--- plugins/olabs/estore/vendor/codeswholesale/sdk/src/CodesWholesale/Http/RequestExecutor.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace CodesWholesale\Http;
-
-
-interface RequestExecutor
-{
-    public function executeRequest(Request $request, $redirectsLimit = 10);
-}
--- plugins/olabs/estore/vendor/codeswholesale/sdk/src/CodesWholesale/Util/Version.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace CodesWholesale\Util;
-
-class Version
-{
-    const SDK_VERSION = '0.0.1.beta';
-}
