f31e74570ad839121c1550e067ea7c07f4640c02
lokendra@lsasoftware.com <lokendra@lsasoftware.com>
2018-04-14 17:41:35 +0530

    fixes

--- plugins/olabs/social/models/EntityRelations.php
+++ plugins/olabs/social/models/EntityRelations.php
@@ -19,4 +19,44 @@ protected $jsonable = ['data'];
      * @var string The database table used by the model.
      */
     public $table = 'olabs_social_entity_relations';
+
+    public function beforeSave(){
+
+         if($this->relation == 'sync' && !$this->status){
+            $this->status = 'L';
+
+         }
+
+    }
+
+
+
+    public function afterSave(){
+
+        if($this->relation == 'sync' && $this->status =='L'){
+               
+               if(isset($this->data['data'])){
+
+                  foreach ($this->data['data'] as $key => $value) {
+                      # code...
+
+                    if(!isset($value['check_in']) || !isset($value['employee_id'])) continue;
+
+                     $entry = $value;
+                     $cer = new EntityRelations();
+                     $cer->created_at = date('Y-m-d H:i:s', strtotime($entry['check_in']) );
+                     $cer->context_type= 'employee';
+                     $cer->relation = 'attendance';
+                     $cer->context_id = $entry['employee_id'];
+                     $cer->data = [$entry];
+                     $cer->save();
+
+                  }
+
+               }
+
+               $this->status = 'O';
+               $this->save();
+        }
+    }
 }
