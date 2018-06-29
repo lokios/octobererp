fbcc30b16306ad21dcb502c6407dc6b7c5e83e81
Amit Srivastava <s.amitsrivastava@gmail.com>
2018-06-29 15:23:10 +0530

    DRP Report in excel

--- plugins/olabs/oims/controllers/Reports.php
+++ plugins/olabs/oims/controllers/Reports.php
@@ -144,7 +144,7 @@ class Reports extends Controller {
         $this->vars['oimsSetting'] = $oimsSetting;
     }
 
-    public function onDprExport() {
+    public function onDprExport_BIN() {
 
 
         //generate PDF html
@@ -240,10 +240,189 @@ td, th { border: 1px solid #ccc; }";
     public function onDprExportExcel() {
 
         $file_type = '.' . post('type');
-//        dd($file_type);
-        $fileName = 'invoices_' . time() . $file_type;
 
-        Excel::excel()->store(new \Olabs\Oims\Exports\DprExport, $fileName, 'local');
+        ////////Generate Excel Data
+        $projectprogress = array();
+        $manpowers = array();
+        $machineries = array();
+        $expenseOnMaterials = array();
+        $expenseOnPcs = array();
+        $total_days = 0;
+
+        $this->vars['grand_total'] = 0;
+        $this->vars['progress_total'] = 0;
+        $this->vars['fix_expense'] = 0;
+
+        if (post('DprSearch')) {
+
+            $searchParams = post('DprSearch');
+
+            // get dpr components
+            $this->searchDPR($searchParams);
+
+            $search_from_date = isset($searchParams['from_date']) ? $searchParams['from_date'] : '';
+            $search_to_date = isset($searchParams['to_date']) ? $searchParams['to_date'] : '';
+//
+            $from_date = false;
+            if ($search_from_date != '') {
+                $from_date = \Olabs\Oims\Models\Settings::convertToDBDate($search_from_date); //date('Y-m-d 00:00:00', strtotime($from_date));
+            }
+
+            $to_date = false;
+            if ($search_to_date != '') {
+                $timeFormat = '23:59:59';
+                $to_date = \Olabs\Oims\Models\Settings::convertToDBDate($search_to_date, $timeFormat);
+            }
+
+            $project = ( trim($searchParams['project']) != "" ) ? $searchParams['project'] : false;
+
+            $projectModal = \Olabs\Oims\Models\Project::find($project);
+        }
+
+        $oimsSetting = \Olabs\Oims\Models\Settings::instance();
+
+        $this->vars['search'] = true;
+
+        $projectprogress = $this->vars['projectprogress'];
+        $manpowers = $this->vars['manpowers'];
+        $machineries = $this->vars['machineries'];
+        $expenseOnMaterials = $this->vars['expenseOnMaterials'];
+        $expenseOnPcs = $this->vars['expenseOnPcs'];
+
+        $total_days = $this->vars['total_days'];
+        $progress_total = 0;
+        $grand_total = 0;
+        $export_data = [];
+
+        //Generating Excel
+        $header_columns = ['Id', 'Project', 'Entry Date', 'Product', 'Quantity', 'Unit Price', 'Total Price'];
+
+        //Project Progress
+        $excel_rows = [];
+        foreach ($projectprogress as $progress) {
+            $products = $progress->products ? $progress->products : array();
+            foreach ($products as $product) {
+                $temp = [];
+                $temp['id'] = $progress->id;
+                $temp['project'] = $progress->project->name;
+                $temp['entry_date'] = date("d-m-Y", strtotime($progress->start_date));
+                $temp['product'] = $product->work ? $product->work->name : '';
+                $temp['quantity'] = $product->quantity;
+                $temp['unit_price'] = $oimsSetting->getPriceFormattedWithoutCurrency($product->unit_price);
+                $temp['total_price'] = $oimsSetting->getPriceFormattedWithoutCurrency($product->total_price);
+                $excel_rows[] = $temp;
+                $progress_total += $product->total_price;
+            }
+        }
+
+        $export_data[] = ['title' => 'Our Billing', 'header' => $header_columns, 'rows' => $excel_rows];
+
+        //Manpower
+        $excel_rows = [];
+        foreach ($manpowers as $manpower) {
+            $products = $manpower->products ? $manpower->products : array();
+            foreach ($products as $product) {
+                $temp = [];
+                $temp['id'] = $manpower->id;
+                $temp['project'] = $manpower->project->name;
+                $temp['entry_date'] = date("d-m-Y", strtotime($manpower->context_date));
+                $temp['product'] = $product->product ? $product->product->title : '';
+                $temp['quantity'] = $product->quantity;
+                $temp['unit_price'] = $oimsSetting->getPriceFormattedWithoutCurrency($product->unit_price);
+                $temp['total_price'] = $oimsSetting->getPriceFormattedWithoutCurrency($product->total_price);
+                $excel_rows[] = $temp;
+                $grand_total += $product->total_price;
+            }
+        }
+        $export_data[] = ['title' => 'Consumed Manpower', 'header' => $header_columns, 'rows' => $excel_rows];
+
+        //Machinery
+        $excel_rows = [];
+        foreach ($machineries as $machinery) {
+            $products = $machinery->products ? $machinery->products : array();
+            foreach ($products as $product) {
+                $temp = [];
+                $temp['id'] = $machinery->id;
+                $temp['project'] = $machinery->project->name;
+                $temp['entry_date'] = date("d-m-Y", strtotime($machinery->context_date));
+                $temp['product'] = $product->product ? $product->product->title : '';
+                $temp['quantity'] = $product->quantity;
+                $temp['unit_price'] = $oimsSetting->getPriceFormattedWithoutCurrency($product->unit_price);
+                $temp['total_price'] = $oimsSetting->getPriceFormattedWithoutCurrency($product->total_price);
+                $excel_rows[] = $temp;
+                $grand_total += $product->total_price;
+            }
+        }
+        $export_data[] = ['title' => 'Machinery Expenditure', 'header' => $header_columns, 'rows' => $excel_rows];
+
+        //Expense On PC
+        $excel_rows = [];
+        foreach ($expenseOnPcs as $expenseOnPc) {
+            $products = $expenseOnPc->products ? $expenseOnPc->products : array();
+            foreach ($products as $product) {
+                $temp = [];
+                $temp['id'] = $expenseOnPc->id;
+                $temp['project'] = $expenseOnPc->project->name;
+                $temp['entry_date'] = date("d-m-Y", strtotime($expenseOnPc->context_date));
+                $temp['product'] = $product->product ? $product->product->title : '';
+                $temp['quantity'] = $product->quantity;
+                $temp['unit_price'] = $oimsSetting->getPriceFormattedWithoutCurrency($product->unit_price);
+                $temp['total_price'] = $oimsSetting->getPriceFormattedWithoutCurrency($product->total_price);
+                $excel_rows[] = $temp;
+                $grand_total += $product->total_price;
+            }
+        }
+        $export_data[] = ['title' => 'Expense Against PC', 'header' => $header_columns, 'rows' => $excel_rows];
+
+        //Expense On Materials
+        $excel_rows = [];
+        foreach ($expenseOnMaterials as $expenseOnMaterial) {
+            $products = $expenseOnMaterial->products ? $expenseOnMaterial->products : array();
+            foreach ($products as $product) {
+                $temp = [];
+                $temp['id'] = $expenseOnMaterial->id;
+                $temp['project'] = $expenseOnMaterial->project->name;
+                $temp['entry_date'] = date("d-m-Y", strtotime($expenseOnMaterial->context_date));
+                $temp['product'] = $product->product ? $product->product->title : '';
+                $temp['quantity'] = $product->quantity;
+                $temp['unit_price'] = $oimsSetting->getPriceFormattedWithoutCurrency($product->unit_price);
+                $temp['total_price'] = $oimsSetting->getPriceFormattedWithoutCurrency($product->total_price);
+                $excel_rows[] = $temp;
+                $grand_total += $product->total_price;
+            }
+        }
+        $export_data[] = ['title' => 'Expense Against Material', 'header' => $header_columns, 'rows' => $excel_rows];
+
+        
+        //Summary
+        $fix_expense = $this->vars['fix_expense'] * $total_days;
+        $total_expense = $fix_expense + $grand_total;
+        $net_profit = $progress_total - $total_expense;
+        $profit_percentage = 0;
+        if($progress_total > 0) {
+            $profit_percentage = $net_profit / $progress_total * 100;
+        }
+//        $html .= $this->makePartial('dpr_summary', ['grand_total' => $this->vars['grand_total'], 'progress_total' => $this->vars['progress_total'], 'fix_expense' => $this->vars['fix_expense'], 'oimsSetting' => $oimsSetting]);
+
+
+
+        $temp = [];
+        $temp['title'] = 'Summary';
+        $temp['header'] = ['DESCRIPTION', 'AMOUNT'];
+        $temp['rows'] = [
+            ['TOTAL EXPENSE (B+C+D+E)', $oimsSetting->getPriceFormattedWithoutCurrency($grand_total)],
+            ["TOTAL FIX EXPENSE (Total Days $total_days)", $oimsSetting->getPriceFormattedWithoutCurrency($fix_expense)],
+            ['GRAND TOTAL EXPENSE', $oimsSetting->getPriceFormattedWithoutCurrency($total_expense)],
+            ['NET WORK DONE(A)', $oimsSetting->getPriceFormattedWithoutCurrency($progress_total)],
+            ['NET PROFIT & LOSS', $oimsSetting->getPriceFormattedWithoutCurrency($net_profit)],
+            ['NET PROFIT & LOSS IN %AGE', $oimsSetting->getPriceFormattedWithoutCurrency($profit_percentage)]
+        ];
+        $export_data[] = $temp;
+        ////////////////////////////////////////////////////////////
+        $fileName = 'dpr_report_' . time() . $file_type;
+
+        Excel::excel()->store(new \Olabs\Oims\Exports\ReportsExport($export_data), $fileName, 'local');
+
         return \Redirect::to('/backend/olabs/oims/reports/download?name=' . $fileName);
     }
 
@@ -257,7 +436,7 @@ td, th { border: 1px solid #ccc; }";
         // Here we can make use of the download response
         $file_name = get('name');
 //        return \Response::download(temp_path($file_name));
-        return \Response::download(storage_path('app'). '/' . $file_name);
+        return \Response::download(storage_path('app') . '/' . $file_name);
     }
 
     protected function searchDPR($searchParams) {
@@ -510,7 +689,7 @@ td, th { border: 1px solid #ccc; }";
         $this->vars['to_date'] = $to_date;
     }
 
-    public function onProgressExport() {
+    public function onProgressExport_BIN() {
 
 
         //generate PDF html
@@ -780,20 +959,20 @@ td, th { border: 1px solid #ccc; }";
             // $profit_percentage = $net_profit / $summary_row['total_spent'] * 100;
             $profit_percentage = $net_profit / $summary_row['total_billing'] * 100;
         }
-        
-        
-        $header_columns = ['Date','Total Amount Spent', 'Billing Amount'];
-        
-        
-        $header_columns =  array_merge($header_columns, $progress_items);
-        
+
+
+        $header_columns = ['Date', 'Total Amount Spent', 'Billing Amount'];
+
+
+        $header_columns = array_merge($header_columns, $progress_items);
+
         $export_data = [];
         $temp = [];
         $temp['title'] = 'Project Progress';
         $temp['header'] = $header_columns;
         $temp['rows'] = $progress_rows;
         $export_data[] = $temp;
-        
+
         $temp = [];
         $temp['title'] = 'Net Profile & Loss';
         $temp['header'] = ['DESCRIPTION', 'AMOUNT'];
@@ -802,7 +981,7 @@ td, th { border: 1px solid #ccc; }";
 
         ////////////////////////////////////////////////////////////
         $fileName = 'progress_report_' . time() . $file_type;
-        
+
         Excel::excel()->store(new \Olabs\Oims\Exports\ReportsExport($export_data), $fileName, 'local');
 
         return \Redirect::to('/backend/olabs/oims/reports/download?name=' . $fileName);
