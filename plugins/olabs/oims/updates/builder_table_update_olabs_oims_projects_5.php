9d4693fa690834f20f2e7ac5ef48d91f78a799ec
Amit Srivastava <s.amitsrivastava@gmail.com>
2018-07-28 15:17:30 +0530

    Project Geo location added

---
+++ plugins/olabs/oims/updates/builder_table_update_olabs_oims_projects_5.php
@@ -0,0 +1,29 @@
+<?php namespace Olabs\Oims\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableUpdateOlabsOimsProjects5 extends Migration
+{
+    public function up()
+    {
+        Schema::table('olabs_oims_projects', function($table)
+        {
+            $table->string('geocode_max_distance')->nullable();
+            $table->string('latitude')->nullable();
+            $table->string('longitude')->nullable();
+            $table->string('geo_required')->nullable();
+        });
+    }
+    
+    public function down()
+    {
+        Schema::table('olabs_oims_projects', function($table)
+        {
+            $table->dropColumn('geocode_max_distance');
+            $table->dropColumn('latitude');
+            $table->dropColumn('longitude');
+            $table->dropColumn('geo_required');
+        });
+    }
+}
