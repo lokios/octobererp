67168f44b8c6bb29af093922debed4248fe9ef44
Amit Srivastava <s.amitsrivastava@gmail.com>
2018-10-05 13:43:09 +0530

    new Plugin : Custom Report Plugin

--- plugins/olabs/widgets/lang/en/lang.php
+++ plugins/olabs/widgets/lang/en/lang.php
@@ -2,10 +2,10 @@
 
 return [
     'plugin' => [
-        'name' => 'Custom Widgets',
+        'name' => 'Custom Report Widgets',
         'widgets' => 'Custom Widgets',
         'main_menu' => 'Widgets',
-        'description' => 'Dynamically add widgets to dashboards',
+        'description' => 'Welcome to custom Report widgets module. Select the custom widget on Widget Configuration menu to view the contents.',
         'access_manage_widgets' => 'Access to Manage Widgets',
         'please_select' => '-- Please select --',
     ],
