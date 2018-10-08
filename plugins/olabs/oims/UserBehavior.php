469d809311f12f0dbaf78ff6586b12e52eaddeca
Amit Srivastava <s.amitsrivastava@gmail.com>
2018-10-08 16:04:00 +0530

    Implemented Backend User banned

---
+++ plugins/olabs/oims/UserBehavior.php
@@ -0,0 +1,54 @@
+<?php namespace Olabs\Oims;
+
+use Backend\Classes\AuthManager;
+use Exception;
+use System\Classes\ModelBehavior;
+
+class UserBehavior extends ModelBehavior
+{
+
+    public function getUserIsBannedAttribute()
+    {
+        if (empty($userId = $this->model->attributes['id'] ?? null)) {
+            return;
+        }
+
+        $throttle = AuthManager::instance()->findThrottleByUserId($userId, null);
+
+        if (!empty($throttle) && $throttle->is_banned) {
+            return true;
+        }
+
+        return false;
+    }
+
+
+    /**
+     * @param $value
+     *
+     * @throws Exception
+     */
+    public function setUserIsBannedAttribute($value)
+    {
+        $loggedUser = AuthManager::instance()->getUser();
+
+        if (empty($userId = $this->model->attributes['id'] ?? null)) {
+            return;
+        }
+
+        if ($value && $loggedUser->id == $userId) {
+            throw new \ApplicationException("You can't ban yourself");
+        }
+
+        $throttle = AuthManager::instance()->findThrottleByUserId($userId, null);
+
+        if ($value) {
+            $throttle->ban();
+
+            return;
+        }
+
+        $throttle->unban();
+    }
+
+}
