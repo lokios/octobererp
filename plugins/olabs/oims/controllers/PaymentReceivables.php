2bb9e03f026197070add4f8d7ac3167f4f3db42e
Amit Srivastava <s.amitsrivastava@gmail.com>
2018-11-16 12:56:37 +0530

    Payment Receivables approval workflow added

--- plugins/olabs/oims/controllers/PaymentReceivables.php
+++ plugins/olabs/oims/controllers/PaymentReceivables.php
@@ -1,7 +1,11 @@
 <?php namespace Olabs\Oims\Controllers;
 
-use Backend\Classes\Controller;
 use BackendMenu;
+use Backend\Classes\Controller;
+use Flash;
+use Log;
+use Backend;
+use Olabs\Oims\Models\Status;
 
 class PaymentReceivables extends Controller
 {
@@ -19,5 +23,215 @@ class PaymentReceivables extends Controller
     {
         parent::__construct();
         BackendMenu::setContext('Olabs.Oims', 'master_register', 'payment_receivables');
+    }
+    
+    
+    public function onSubmitForApproval() {
+        $id = post('id');
+
+        $model = \Olabs\Oims\Models\PaymentReceivable::find($id);
+
+        $model->comment = post('comment');
+
+        $msg = $model->onSubmitForApproval();
+
+        if ($msg['s']) {
+
+            Flash::success($msg['m']);
+            $redirectUrl = 'olabs/oims/paymentreceivables/preview/' . $id;
+            return Backend::redirect($redirectUrl);
+        } else {
+            Flash::warning($msg['m']);
+        }
+
+        return ["#object-status" => $model->objectstatus->name];
+    }
+
+    public function onApproved() {
+        $id = post('id');
+
+        $model = \Olabs\Oims\Models\PaymentReceivable::find($id);
+
+        $model->comment = post('comment');
+
+        $msg = $model->onApproved();
+
+        if ($msg['s']) {
+            Flash::success($msg['m']);
+
+            $redirectUrl = 'olabs/oims/paymentreceivables/preview/' . $id;
+            return Backend::redirect($redirectUrl);
+        } else {
+            Flash::warning($msg['m']);
+        }
+
+        return ["#object-status" => $model->objectstatus->name];
+    }
+
+    public function onRejected() {
+        $id = post('id');
+
+        $model = \Olabs\Oims\Models\PaymentReceivable::find($id);
+
+        $model->comment = post('comment');
+
+        $msg = $model->onRejected();
+
+        if ($msg['s']) {
+            Flash::success($msg['m']);
+
+            $redirectUrl = 'olabs/oims/paymentreceivables/preview/' . $id;
+            return Backend::redirect($redirectUrl);
+        } else {
+            Flash::warning($msg['m']);
+        }
+
+        return ["#object-status" => $model->objectstatus->name];
+    }
+
+    public function onSubmitForHOApproval() {
+        $id = post('id');
+
+        $model = \Olabs\Oims\Models\PaymentReceivable::find($id);
+
+        $model->comment = post('comment');
+
+        $msg = $model->onSubmitForHOApproval();
+
+        if ($msg['s']) {
+
+            Flash::success($msg['m']);
+            $redirectUrl = 'olabs/oims/paymentreceivables/preview/' . $id;
+            return Backend::redirect($redirectUrl);
+        } else {
+            Flash::warning($msg['m']);
+        }
+
+        return ["#object-status" => $model->objectstatus->name];
+    }
+
+    public function onHOApproved() {
+        $id = post('id');
+
+        $model = \Olabs\Oims\Models\PaymentReceivable::find($id);
+
+        $model->comment = post('comment');
+
+        $msg = $model->onHOApproved();
+
+        if ($msg['s']) {
+            Flash::success($msg['m']);
+
+            $redirectUrl = 'olabs/oims/paymentreceivables/preview/' . $id;
+            return Backend::redirect($redirectUrl);
+        } else {
+            Flash::warning($msg['m']);
+        }
+
+        return ["#object-status" => $model->objectstatus->name];
+    }
+
+    public function onHORejected() {
+        $id = post('id');
+
+        $model = \Olabs\Oims\Models\PaymentReceivable::find($id);
+
+        $model->comment = post('comment');
+
+        $msg = $model->onHORejected();
+
+        if ($msg['s']) {
+            Flash::success($msg['m']);
+
+            $redirectUrl = 'olabs/oims/paymentreceivables/preview/' . $id;
+            return Backend::redirect($redirectUrl);
+        } else {
+            Flash::warning($msg['m']);
+        }
+
+        return ["#object-status" => $model->objectstatus->name];
+    }
+    
+    
+    //handle back date entry :
+
+    public function formExtendFields($form) {
+
+        //Get if field have already value (EDIT MODE)
+        $contextDate = isset($form->data->attributes['context_date']) ? $form->data->attributes['context_date'] : false;
+        if ($form->removeField('context_date')) {
+
+
+            // Handle context date field        
+            if ($this->user->isAdmin() OR $this->user->hasAccess('olabs.oims.record_back_date_entry')) {
+                //Is Admin OR have back Date date entry permission
+                $form->addFields([
+                    'context_date' => [
+                        'label' => 'Date',
+                        'oc.commentPosition' => '',
+                        'mode' => 'date',
+                        'span' => 'auto',
+                        'format' => 'd/m/Y',
+                        'required' => 1,
+                        'type' => 'datepicker',
+                        'default' => 'today',
+                        'attributes' => [
+                        ]
+                    ]
+                ]);
+            } else {
+                //Dont have back date entry permission
+                if ($contextDate && date('Y-m-d H:i:s', strtotime($contextDate)) >= date('Y-m-d 00:00:00', strtotime('today'))) {
+                    //Make Readonly
+                    $form->addFields([
+                        'context_date' => [
+                            'label' => 'Date',
+                            'oc.commentPosition' => '',
+                            'mode' => 'date',
+                            'span' => 'auto',
+                            'default' => 'today',
+                            'format' => 'd/m/Y',
+                            'required' => 1,
+                            'type' => 'datepicker',
+                            'minDate' => 'today',
+                            'attributes' => [
+                            ]
+                        ]
+                    ]);
+                } else if (!$contextDate) {
+                    $form->addFields([
+                        'context_date' => [
+                            'label' => 'Date',
+                            'oc.commentPosition' => '',
+                            'mode' => 'date',
+                            'span' => 'auto',
+                            'default' => 'today',
+                            'format' => 'd/m/Y',
+                            'required' => 1,
+                            'type' => 'datepicker',
+                            'minDate' => 'today',
+                            'attributes' => [
+                            ]
+                        ]
+                    ]);
+                } else {
+                    $form->addFields([
+                        'context_date' => [
+                            'label' => 'Date',
+                            'oc.commentPosition' => '',
+                            'mode' => 'date',
+                            'span' => 'auto',
+                            'default' => 'today',
+                            'format' => 'd/m/Y',
+                            'required' => 1,
+                            'type' => 'datepicker',
+                            'attributes' => [
+                                'disabled' => true,
+                            ]
+                        ]
+                    ]);
+                }
+            }
+        }
     }
 }
