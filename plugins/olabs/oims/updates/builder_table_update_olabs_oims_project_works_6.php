e774ae293676275a55ab40245fe79ea211493f58
Amit Srivastava <s.amitsrivastava@gmail.com>
2019-02-16 14:17:08 +0530

    Updates

---
+++ plugins/olabs/oims/updates/builder_table_update_olabs_oims_project_works_6.php
@@ -0,0 +1,25 @@
+<?php namespace Olabs\Oims\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableUpdateOlabsOimsProjectWorks6 extends Migration
+{
+    public function up()
+    {
+        Schema::table('olabs_oims_project_works', function($table)
+        {
+            $table->decimal('labour_coefficient', 10, 0)->nullable();
+            $table->decimal('labour_unit_price', 10, 0)->nullable();
+        });
+    }
+    
+    public function down()
+    {
+        Schema::table('olabs_oims_project_works', function($table)
+        {
+            $table->dropColumn('labour_coefficient');
+            $table->dropColumn('labour_unit_price');
+        });
+    }
+}
