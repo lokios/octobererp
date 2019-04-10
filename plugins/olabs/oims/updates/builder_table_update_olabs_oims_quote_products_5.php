967f015e49387d21b37b9f9a54bdc135c9064b35
Amit Srivastava <s.amitsrivastava@gmail.com>
2019-04-10 19:48:28 +0530

    Updates

---
+++ plugins/olabs/oims/updates/builder_table_update_olabs_oims_quote_products_5.php
@@ -0,0 +1,23 @@
+<?php namespace Olabs\Oims\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableUpdateOlabsOimsQuoteProducts5 extends Migration
+{
+    public function up()
+    {
+        Schema::table('olabs_oims_quote_products', function($table)
+        {
+            $table->text('description')->nullable();
+        });
+    }
+    
+    public function down()
+    {
+        Schema::table('olabs_oims_quote_products', function($table)
+        {
+            $table->dropColumn('description');
+        });
+    }
+}
---
+++ plugins/olabs/oims/updates/builder_table_update_olabs_oims_quotes_19.php
@@ -0,0 +1,23 @@
+<?php namespace Olabs\Oims\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableUpdateOlabsOimsQuotes19 extends Migration
+{
+    public function up()
+    {
+        Schema::table('olabs_oims_quotes', function($table)
+        {
+            $table->text('operational_terms')->nullable();
+        });
+    }
+    
+    public function down()
+    {
+        Schema::table('olabs_oims_quotes', function($table)
+        {
+            $table->dropColumn('operational_terms');
+        });
+    }
+}
