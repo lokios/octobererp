7b93fe868ae2d0e3835ea57cec1dd5a4a704e19b
Amit Srivastava <s.amitsrivastava@gmail.com>
2019-07-22 17:03:22 +0530

    Updates

--- plugins/olabs/oims/models/Unit.php
+++ plugins/olabs/oims/models/Unit.php
@@ -1,16 +1,19 @@
-<?php namespace Olabs\Oims\Models;
+<?php
+
+namespace Olabs\Oims\Models;
 
 use Model;
 use DB;
 use Lang;
 use BackendAuth;
+
 /**
  * Model
  */
-class Unit extends BaseModel
-{
+class Unit extends BaseModel {
+
     use \October\Rain\Database\Traits\Validation;
-    
+
     use \October\Rain\Database\Traits\SoftDelete;
     use \October\Rain\Database\Traits\Sluggable;
 
@@ -20,7 +23,7 @@ class Unit extends BaseModel
      * @var array Generate slugs for these attributes.
      */
     protected $slugs = ['slug' => 'name'];
-    
+
     /*
      * Validation
      */
@@ -30,9 +33,8 @@ class Unit extends BaseModel
             'alpha_dash',
             'between:1,255',
             'unique:olabs_oims_units',
-        ],      
+        ],
     ];
-    
     protected $primaryKey = 'slug';
     public $incrementing = false;
 
@@ -40,42 +42,41 @@ class Unit extends BaseModel
      * @var string The database table used by the model.
      */
     public $table = 'olabs_oims_units';
-    
     protected $jsonable = ['conversion_meta'];
-    
-    public function beforeSave()
-    {
+
+    public function beforeSave() {
         // Autogenerate name
 //        if (empty($this->name)) {
 //            $this->name = $this->make . ' ' . $this->model . ' ' . $this->variant;
 //        }
-
         // Force creation of slug
         if (empty($this->slug)) {
             unset($this->slug);
             $this->setSluggedValue('slug', 'name');
         }
     }
-    
+
     public function getUnitOptions() {
         $list = Unit::select(
                         DB::raw("name, slug")
                 )->where('status', '1')->lists('name', 'slug');
         return [null => Lang::get("olabs.oims::lang.plugin.please_select")] + $list;
     }
-    
-    
-    public function getConversions($value){
+
+    public function getConversions($value) {
         $units = $this->getUnitOptions();
 //        dd($units);
         $list = [];
-        $temp = ['unit'=> $this->slug, 'name' => $this->name, 'value' =>$value, 'conversion' => '1'];
+        $temp = ['unit' => $this->slug, 'name' => $this->name, 'value' => $value, 'conversion' => '1'];
         $list[] = $temp;
-        foreach($this->conversion_meta as $meta){
-            $temp = ['unit'=> $meta['unit'], 'name' => $units[$meta['unit']], 'value' =>$value * $meta['conversion'], 'conversion' => $meta['conversion']];
-            $list[] = $temp;
+        if ($this->conversion_meta) {
+            foreach ($this->conversion_meta as $meta) {
+                $temp = ['unit' => $meta['unit'], 'name' => $units[$meta['unit']], 'value' => $value * $meta['conversion'], 'conversion' => $meta['conversion']];
+                $list[] = $temp;
+            }
         }
+
         return $list;
     }
-    
+
 }
