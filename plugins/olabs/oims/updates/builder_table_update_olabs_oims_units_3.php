399a35d88ff7a27b85bcea278cd0c2467cce6963
Amit Srivastava <s.amitsrivastava@gmail.com>
2019-07-22 14:21:37 +0530

    Updates

---
+++ plugins/olabs/oims/updates/builder_table_update_olabs_oims_units_3.php
@@ -0,0 +1,23 @@
+<?php namespace Olabs\Oims\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableUpdateOlabsOimsUnits3 extends Migration
+{
+    public function up()
+    {
+        Schema::table('olabs_oims_units', function($table)
+        {
+            $table->text('conversion_meta')->nullable();
+        });
+    }
+    
+    public function down()
+    {
+        Schema::table('olabs_oims_units', function($table)
+        {
+            $table->dropColumn('conversion_meta');
+        });
+    }
+}
